67fd71663fca7d79007d9b1b124deaf9be7339db
erfanbayaat <ebayat789@gmail.com>
2025-06-12 19:42:16 +0330

    Delete Users.js

--- Users.js
+++
@@ -1,8 +0,0 @@
-const Users = [
-  { id: 1, name: "Ali", age: 25, job: "Developer", city: "Tehran" },
-  { id: 2, name: "Sara", age: 30, job: "Designer", city: "Shiraz" },
-  { id: 3, name: "Reza", age: 22, job: "Student", city: "Tabriz" },
-  { id: 4, name: "Niloofar", age: 28, job: "Writer", city: "Mashhad" },
-]
-
-export default Users
