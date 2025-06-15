cc52e7051801424a5a9441b70e607f839d4597a9
erfanbayaat <ebayat789@gmail.com>
2025-06-15 07:27:13 +0330

    Update Square.jsx

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,10 @@
-export default function Square({ value, onSquareClick }) {
+import React from "react";
+
+export default function Square({ value, onClick }) {
   return (
     <button
       className="w-20 h-20 text-3xl border border-gray-400 flex items-center justify-center hover:bg-gray-100 transition"
-      onClick={onSquareClick}
+      onClick={onClick}
     >
       {value}
     </button>
