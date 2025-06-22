1481a237490576592a97d30f11e89c21805de046
erfanbayaat <ebayat789@gmail.com>
2025-06-22 07:47:13 +0330

    the seconde try to fix buggs for display

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TrafficLight from './components/trafficLight/trafficLight';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Traffic Light</h1>
+      <TrafficLight />
+    </div>
+  );
+}
+
+export default App;
